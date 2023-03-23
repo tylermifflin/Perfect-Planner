1b4ccd964f70c9535bba8208eec1fa494fc7869e
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-23 17:15:36 -0600

    think i figured out why it wasn't working, needed to remove the current classes past, present, and future from the HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var currentDayEl = $("#currentDay");
-var saveBtnEl = $(".saveBtn");
-var timeBlockEl = $(".time-block");
-var textAreaEl = $(".description");
-var hourEl = $(".hour");
-var hour9El = $("#hour-9");
-var hour10El = $("#hour-10");
-var hour11El = $("#hour-11");
-var hour12El = $("#hour-12");
-var hour13El = $("#hour-13");
-var hour14El = $("#hour-14");
-var hour15El = $("#hour-15");
-var hour16El = $("#hour-16");
-var hour17El = $("#hour-17");
-var pastEl = $(".past");
-var presentEl = $(".present");
-var futureEl = $(".future");
+var currentDayEl = $('#currentDay');
+var saveBtnEl = $('saveBtn');
+var timeBlockEl = $('time-block');
+var textAreaEl = $('description');
+var hourEl = $('hour');
+var hour9El = $('#hour-9');
+var hour10El = $('#hour-10');
+var hour11El = $('#hour-11');
+var hour12El = $('#hour-12');
+var hour13El = $('#hour-13');
+var hour14El = $('#hour-14');
+var hour15El = $('#hour-15');
+var hour16El = $('#hour-16');
+var hour17El = $('#hour-17');
+var pastEl = $('past');
+var presentEl = $('present');
+var futureEl = $('future');
 // q: how to get the current hour in 24-hour time?
 // a: dayjs().format('H')
 function displayDate () {
@@ -27,6 +27,9 @@ function displayDate () {
 function changetimeclass () { 
   var currentTime = dayjs().hour();
   console.log(currentTime);
+  if (currentTime > 15) {
+    hour15El.addClass('past');
+  }
 }
 
 
